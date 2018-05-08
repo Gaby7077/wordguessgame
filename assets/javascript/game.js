153d4d761f222ddf552f8a69fc080d29bdc33427
Gaby7077 <gamma@gmail.com>
2018-05-08 12:12:18 -0500

    Homework3

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+
+
+var name = ["Keroppi","Chococat","Kuromi","Sugarbunnies","Kirimichan","Gudetama"];
+var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "u", "v", "w", "x", "y", "z"];
+var tries = 7;
+var lettersFound = []
+var currentWord = name[Math.floor((Math.random () * name.length)+1)];
+
+
+
+
+
+
